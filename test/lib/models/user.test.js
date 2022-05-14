c80ab6d394a8c9588ce2b65f11ea28185a931d2c
felix <josevelazco134@gmail.com>
2022-05-14 11:15:05 -0600

    Creamos pruebas para setters y getters

--- test/lib/models/user.test.js
+++ test/lib/models/user.test.js
@@ -1,7 +1,28 @@
-const User = require("./../../../lib/models/user")
-describe('Users',() =>{
-  test('1) Create user', () => {
-    const result = 1 + 2;
-    expect(result).toBe(10);
-  })
-})
+const User = require("./../../../lib/models/user");
+describe("Users",() =>{
+	test("1) Create user", () => {
+		const user = new User("nombre", "apellido", "correo@correo.mx","1234", "mexico", "chihuahua");
+    
+		expect(user).not.toBeNull();
+    expect(user.getName).toBe("nombre");
+    expect(user.getLastname).toBe("apellido");
+    expect(user.getEmail).toBe("correo@correo.mx");
+    expect(user.getPassword).toBe("1234");
+    expect(user.getCountry).toBe("mexico");
+    expect(user.getState).toBe("chihuahua");
+    expect(user.getState).not.toBeNull();
+
+	});
+	test("1) Test setters", () => {
+		const user = new User("nombre", "apellido", "correo@correo.mx","1234", "mexico", "chihuahua");
+    user.setEmail = "newCorreo@correo.mx"
+    user.setPassword = "5678"
+    user.setCountry = "japon"
+    user.setState = "osaka"
+
+		expect(user.getEmail).toBe("newCorreo@correo.mx");
+		expect(user.getPassword).toBe("5678");
+		expect(user.getCountry).toBe("japon");
+		expect(user.getState).toBe("osaka");
+	});
+});
