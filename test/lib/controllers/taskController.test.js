5eabc46bc0df210e6257f42f5fe2553bb7a50907
felix <josevelazco134@gmail.com>
2022-05-15 00:51:25 -0600

    Corremos eslint

--- test/lib/controllers/taskController.test.js
+++ test/lib/controllers/taskController.test.js
@@ -1,16 +1,16 @@
 const TaskController = require("./../../../lib/controllers/taskController");
 const taskObj = {
 	id: 1,
-    description: "description",
-    points: 20,
-    itsDone: false,
-    itsAvailable: true,
-}
+	description: "description",
+	points: 20,
+	itsDone: false,
+	itsAvailable: true,
+};
 
 describe("Task Controller",() =>{
 	test("1) Create task", () => { 
 		const task = TaskController.createTask(taskObj);
-        console.log(task);
+		console.log(task);
 		expect(task).not.toBeNull();
 		expect(task.id).toBe(1);
 		expect(task.description).toBe("description");
